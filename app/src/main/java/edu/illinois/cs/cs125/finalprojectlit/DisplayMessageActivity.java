c1ada48bca5056267f62f137219ca093ddb32d33
lifeichen <lifeic2@illinois.edu>
2018-04-27 22:06:24 -0500

    Fixed the api call and another displaymessage activity (simpliy change the layout version into v-16)

--- app/src/main/java/edu/illinois/cs/cs125/finalprojectlit/DisplayMessageActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/finalprojectlit/DisplayMessageActivity.java
@@ -3,35 +3,13 @@ package edu.illinois.cs.cs125.finalprojectlit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class DisplayMessageActivity extends AppCompatActivity {
-    private static RequestQueue requestQueue;
-    private static final String TAG = "FInalProjectlit:Main";
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-//        Button b = (Button)findViewById(R.id.button);
-//        b.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//               //startAPICall();
-//            }
-//        });
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
@@ -40,34 +18,5 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
-        startAPICall();
     }
-    void startAPICall() {
-        try {
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                    Request.Method.GET,
-                    "https://api.foursquare.com/v2/venues/explore?near=NYC&oauth_token=XYJVATATPRASIQPNCCSDKSEHXHBJLFVL4J43RRUR32P55S5J&v=20180424",
-                    null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(final JSONObject response) {
-                            try {
-                                final TextView find = findViewById(R.id.textView);
-                                find.setText(response.toString());
-                                Log.d(TAG, response.toString(2));
-                            } catch (JSONException ignored) {
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(final VolleyError error) {
-                    Log.e(TAG, error.toString());
-                }
-            });
-            requestQueue.add(jsonObjectRequest);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 }
--- app/src/main/java/edu/illinois/cs/cs125/finalprojectlit/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/finalprojectlit/MainActivity.java
@@ -1,17 +1,13 @@
 package edu.illinois.cs.cs125.finalprojectlit;
 
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.android.volley.Request;
@@ -21,20 +17,76 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.json.JSONObject;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+/**
+ * Main screen for our API testing app.
+ */
+public final class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "finalprojectlit:Main";
 
-public class MainActivity extends AppCompatActivity {
+    /** Request queue for our network requests. */
     private static RequestQueue requestQueue;
-    private static final String TAG = "FInalProjectlit:Main";
+
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    /**
+     * Run when our activity comes into view.
+     *
+     * @param savedInstanceState state that was saved by the activity last time it was paused
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set up a queue for our Volley requests
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Load the main layout for our activity
         setContentView(R.layout.activity_main);
+
+        // Attach the handler to our UI button
+        final Button startAPICall = findViewById(R.id.startAPICall);
+        startAPICall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Start API button clicked");
+                startAPICall();
+            }
+        });
+
+        // Make sure that our progress bar isn't spinning and style it a bit
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    /**
+     * Make an API call.
+     */
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=4PQSM3203ZN13XY2GJCTH2ZBPU43KZGJXNJ4YOLDJ4OX4GDM&client_secret=VQP3TG3GKJPBHWYSASZJ1PCAZJFRBPGGW1VPEUXV0FJ2YYIX&v=20180427",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            final TextView find = findViewById(R.id.jsonResult);
+                            find.setText(response.toString());
+                            Log.d(TAG, response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
     /** Called when the user taps the getlit button */
     public void sendMessage(View view) {
@@ -44,30 +96,5 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
-    void startAPICall() {
-        try {
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                    Request.Method.GET,
-                    "https://api.foursquare.com/v2/venues/explore?near=NYC&oauth_token=XYJVATATPRASIQPNCCSDKSEHXHBJLFVL4J43RRUR32P55S5J&v=20180424",
-                    null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(final JSONObject response) {
-                            try {
-                                final TextView find = findViewById(R.id.textView);
-                                find.setText(response.toString());
-                                Log.d(TAG, response.toString(2));
-                            } catch (JSONException ignored) { }
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(final VolleyError error) {
-                    Log.e(TAG, error.toString());
-                }
-            });
-            requestQueue.add(jsonObjectRequest);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
 }
